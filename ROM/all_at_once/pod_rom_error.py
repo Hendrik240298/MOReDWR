0e19b032478d9d224872171ae31ca341bf73fe6c
Julian Roth <julian.roth1998@gmx.de>
2022-10-14 11:14:05 +0200

    Finished first draft of Tensor-Product Space-Time POD-ROM

--- ROM/all_at_once/pod_rom_error.py
+++ ROM/all_at_once/pod_rom_error.py
@@ -7,6 +7,7 @@ import os
 
 PLOTTING = True
 INTERPOLATION_TYPE = "nearest"  # "linear", "cubic"
+LOAD_PRIMAL_SOLUTION = False
 OUTPUT_PATH = "../../FOM/all_at_once/output/dim=1/"
 
 for cycle in os.listdir(OUTPUT_PATH):
@@ -42,7 +43,7 @@ for cycle in os.listdir(OUTPUT_PATH):
     n_dofs = {"space": coordinates_x.shape[0], "time": coordinates_t.shape[0]}
 
     # primal solution
-    primal_solution = np.loadtxt(OUTPUT_PATH + cycle + "/solution.txt")
+    primal_solution = np.loadtxt(OUTPUT_PATH + cycle + "/solution.txt") if LOAD_PRIMAL_SOLUTION else scipy.sparse.linalg.spsolve(matrix_bc, rhs_bc)
     if PLOTTING:
         grid_t, grid_x = np.mgrid[0:4:100j, 0:1:100j]
         primal_grid = scipy.interpolate.griddata(
@@ -116,6 +117,7 @@ for cycle in os.listdir(OUTPUT_PATH):
     # determine number of POD basis vectors needed to preserve certain ratio of energy
     r = np.sum([(eigen_values[:i].sum() / eigen_values.sum()) <
                ENERGY_RATIO_THRESHOLD for i in range(n_dofs["time"])])
+    # FOR DEBUGGING: r = 2
     print(
         f"To preserve {ENERGY_RATIO_THRESHOLD} of information we need {r} POD vector(s). (result: {eigen_values[:r].sum() / eigen_values.sum()} information).")
 
@@ -133,6 +135,45 @@ for cycle in os.listdir(OUTPUT_PATH):
         plt.show()
 
     # change from the FOM to the POD basis
+    space_time_pod_basis = scipy.sparse.block_diag([pod_basis]*n_dofs["time"]) # NOTE: this might be a bit inefficient
+    plt.spy(space_time_pod_basis, markersize=2)
+    plt.show()
+    
+    reduced_system_matrix = space_time_pod_basis.T.dot(matrix_no_bc.dot(space_time_pod_basis))
+    reduced_rhs = space_time_pod_basis.T.dot(rhs_no_bc)
+    
+    # solve reduced linear system
+    reduced_solution = scipy.sparse.linalg.spsolve(reduced_system_matrix, reduced_rhs)
+    #print(reduced_solution.shape)
+    true_reduced_solution = space_time_pod_basis.T.dot(primal_solution)
+    #print(true_reduced_solution.reshape(-1,r)[:,0:1])
+    #print(scipy.sparse.block_diag([pod_basis[:,0:1]]*n_dofs["time"]).T.dot(primal_solution))
+    #print(space_time_pod_basis.shape)
+    #print(true_reduced_solution.shape)
+    print(f"Reduced solution is wrong by a factor of {reduced_solution[0]/true_reduced_solution[0]}")
+    
+    projected_reduced_solution = space_time_pod_basis.dot(reduced_solution)
+    if PLOTTING:
+      grid_t, grid_x = np.mgrid[0:4:100j, 0:1:100j]
+      reduced_grid = scipy.interpolate.griddata(coordinates, projected_reduced_solution, (grid_t, grid_x), method=INTERPOLATION_TYPE)
+      error_grid = scipy.interpolate.griddata(coordinates, projected_reduced_solution-primal_solution, (grid_t, grid_x), method=INTERPOLATION_TYPE)
+      fig, axs = plt.subplots(2)
+      fig.suptitle(f"Projected reduced solution (ref={cycle.split('=')[1]})")
+      im0 = axs[0].imshow(reduced_grid.T, extent=(0, 4, 0, 1), origin='lower')
+      axs[0].set_xlabel("$t$")
+      axs[0].set_ylabel("$x$")
+      axs[0].set_title("$u_r$")
+      fig.colorbar(im0, ax=axs[0])
+      im1 = axs[1].imshow(error_grid.T, extent=(0, 4, 0, 1), origin='lower')
+      axs[1].set_xlabel("$t$")
+      axs[1].set_ylabel("$x$")
+      axs[1].set_title("$u_h - u_r$")
+      fig.colorbar(im1, ax=axs[1])
+      plt.show()
+
+    
+    
+    """
     reduced_system_matrix = scipy.sparse.dok_matrix(
         (r*n_dofs["time"], r*n_dofs["time"]), dtype=np.float32)
     reduced_rhs = np.zeros(r*n_dofs["time"], dtype=np.float32)
@@ -171,6 +212,7 @@ for cycle in os.listdir(OUTPUT_PATH):
         np.dot(pod_basis.T, system_matrix.array()), pod_basis)
     reduced_rhs_matrix = np.dot(
         np.dot(pod_basis.T,    rhs_matrix.array()), pod_basis)
+    """
 
     # TODO: primal POD-ROM
     # TODO: evaluate J(u_h) - J(u_r)
