adcf98d0aa4b0b0843e12d9c5995df1ce4992432
Hendrik Fischer <github@fischerhendrik.de>
2022-10-17 10:50:24 +0200

    added dual reduction + plotting

--- ROM/all_at_once/pod_rom_error.py
+++ ROM/all_at_once/pod_rom_error.py
@@ -70,7 +70,6 @@ for cycle in os.listdir(OUTPUT_PATH):
 
     # goal functionals
     J = {"u_h": np.dot(dual_rhs_no_bc, primal_solution), "u_r": []}
-    print("J(u_h) =", J["u_h"])
 
     # -------------- #
     # PRIMAL POD-ROM #
@@ -81,25 +80,32 @@ for cycle in os.listdir(OUTPUT_PATH):
     # ---------------
     # snapshot matrix
     Y = primal_solution.reshape(n_dofs["time"], n_dofs["space"]).T
+    Y_dual = dual_solution.reshape(n_dofs["time"], n_dofs["space"]).T
 
     # ------------------
     # correlation matrix
     # TODO: include mass matrix
     correlation_matrix = np.dot(Y.T, Y)
+    dual_correlation_matrix = np.dot(Y_dual.T, Y_dual)
     assert correlation_matrix.shape == (
         n_dofs["time"], n_dofs["time"]), f"Matrix should be of #DoFs(time) shape"
 
     # -----------------------------------------
     # compute eigenvalues of correlation matrix
     eigen_values, eigen_vectors = np.linalg.eig(correlation_matrix)
+    dual_eigen_values, dual_eigen_vectors = np.linalg.eig(dual_correlation_matrix)
 
     # ignore the complex part of the eigen values and eigen vectors
     eigen_values, eigen_vectors = eigen_values.astype(
         'float64'), eigen_vectors.astype('float64')
+    dual_eigen_values, dual_eigen_vectors = dual_eigen_values.astype(
+        'float64'), dual_eigen_vectors.astype('float64')
 
     # sort the eigenvalues in descending order
     eigen_values, eigen_vectors = eigen_values[eigen_values.argsort(
     )[::-1]], eigen_vectors[eigen_values.argsort()[::-1]]
+    dual_eigen_values, dual_eigen_vectors = dual_eigen_values[dual_eigen_values.argsort(
+    )[::-1]], dual_eigen_vectors[eigen_values.argsort()[::-1]]
 
     # plot eigen value decay
     if PLOTTING:
@@ -110,28 +116,50 @@ for cycle in os.listdir(OUTPUT_PATH):
         plt.ylabel("eigen value")
         plt.title("Eigenvalues of correlation matrix")
         plt.show()
+        
+        plt.plot(
+            range(1, min(n_dofs["space"], n_dofs["time"])+1), dual_eigen_values)
+        plt.yscale("log")
+        plt.xlabel("index")
+        plt.ylabel("eigen value")
+        plt.title("Dual eigenvalues of correlation matrix")
+        plt.show()
 
     # desired energy ratio
-    ENERGY_RATIO_THRESHOLD = 0.999
+    ENERGY_RATIO_THRESHOLD = 0.9999
 
     # determine number of POD basis vectors needed to preserve certain ratio of energy
     r = np.sum([(eigen_values[:i].sum() / eigen_values.sum()) <
                ENERGY_RATIO_THRESHOLD for i in range(n_dofs["time"])])
     # FOR DEBUGGING: r = 2
+    r_dual = np.sum([(dual_eigen_values[:i].sum() / dual_eigen_values.sum()) <
+               ENERGY_RATIO_THRESHOLD for i in range(n_dofs["time"])])
+    
     print(
-        f"To preserve {ENERGY_RATIO_THRESHOLD} of information we need {r} POD vector(s). (result: {eigen_values[:r].sum() / eigen_values.sum()} information).")
+        f"To preserve {ENERGY_RATIO_THRESHOLD} of information we need {r} primal POD vector(s). (result: {eigen_values[:r].sum() / eigen_values.sum()} information).")
+    print(
+        f"To preserve {ENERGY_RATIO_THRESHOLD} of information we need {r_dual} dual POD vector(s). (result: {dual_eigen_values[:r_dual].sum() / dual_eigen_values.sum()} information).")
 
     # -------------------------
     # compute POD basis vectors
     pod_basis = np.dot(np.dot(Y, eigen_vectors[:, :r]), np.diag(
         1. / np.sqrt(eigen_values[:r])))
+    
+    dual_pod_basis = np.dot(np.dot(Y_dual, dual_eigen_vectors[:, :r_dual]), np.diag(
+        1. / np.sqrt(dual_eigen_values[:r_dual])))
 
     # plot the POD basis
     if PLOTTING:
         plt.plot(coordinates_x, pod_basis[:, 0], color="blue")
         #plt.plot(coordinates_x, pod_basis[:, 1], color="red")
         plt.xlabel("$x$")
-        plt.title("POD vectors")
+        plt.title("Primal POD vectors")
+        plt.show()
+
+        plt.plot(coordinates_x, dual_pod_basis[:, 0], color="blue")
+        plt.plot(coordinates_x, dual_pod_basis[:, 1], color="red")
+        plt.xlabel("$x$")
+        plt.title("Dual POD vectors")
         plt.show()
 
     # change from the FOM to the POD basis
@@ -145,18 +173,22 @@ for cycle in os.listdir(OUTPUT_PATH):
     # solve reduced linear system
     reduced_solution = scipy.sparse.linalg.spsolve(reduced_system_matrix, reduced_rhs)
     #print(reduced_solution.shape)
-    true_reduced_solution = space_time_pod_basis.T.dot(primal_solution)
+    #true_reduced_solution = space_time_pod_basis.T.dot(primal_solution)
     #print(true_reduced_solution.reshape(-1,r)[:,0:1])
     #print(scipy.sparse.block_diag([pod_basis[:,0:1]]*n_dofs["time"]).T.dot(primal_solution))
     #print(space_time_pod_basis.shape)
     #print(true_reduced_solution.shape)
-    print(f"Reduced solution is wrong by a factor of {reduced_solution[0]/true_reduced_solution[0]}")
+    #print(f"Reduced solution is wrong by a factor of {reduced_solution[0]/true_reduced_solution[0]}")
     
     projected_reduced_solution = space_time_pod_basis.dot(reduced_solution)
+    J["u_r"].append(np.dot(dual_rhs_no_bc, projected_reduced_solution))
+    print("J(u_h) =", J["u_h"])
+    print("J(u_r) =", J["u_r"][-1]) # TODO: in the future compare J(u_r) for different values of r
+    
     if PLOTTING:
       grid_t, grid_x = np.mgrid[0:4:100j, 0:1:100j]
       reduced_grid = scipy.interpolate.griddata(coordinates, projected_reduced_solution, (grid_t, grid_x), method=INTERPOLATION_TYPE)
-      error_grid = scipy.interpolate.griddata(coordinates, projected_reduced_solution-primal_solution, (grid_t, grid_x), method=INTERPOLATION_TYPE)
+      error_grid = scipy.interpolate.griddata(coordinates, np.abs(projected_reduced_solution-primal_solution), (grid_t, grid_x), method=INTERPOLATION_TYPE)
       fig, axs = plt.subplots(2)
       fig.suptitle(f"Projected reduced solution (ref={cycle.split('=')[1]})")
       im0 = axs[0].imshow(reduced_grid.T, extent=(0, 4, 0, 1), origin='lower')
